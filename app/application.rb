bfa311815455121d1ff4994d4867ff802df296f9
Charles <charlesangeloplai@gmail.com>
2020-12-26 16:58:56 -0500

    commit tree 3cb144ac8545a088e13105f1cf067178e10d7c19

---
+++ app/application.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class Application
+    def call(env)
+        @@items = [
+            Item.new("Figs", 3.42), 
+            Item.new("Pears", 0.99)
+        ]
+
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
+
+        if req.path.match(/items/)
+            item = req.path.split("/items/").last
+            
+            if item = @@items.find{|i| i.name == item}
+                resp.write item.price
+            else
+                resp.write "Item not found"
+                resp.status = 400
+            end
+        else
+            resp.write "Route not found"
+            resp.status = 404
+        end
+
+        resp.finish
+    end
+end
